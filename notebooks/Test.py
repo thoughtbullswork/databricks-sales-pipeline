4e6ba0fe205a67a8cfa335600aa5d04126e4fceb
Narendra Kashyap <thoughtbullswork@gmail.com>
2026-01-02 09:01:51 +0530

    Revert "add a test file"
    This reverts commit 4520e64b6d17336954e4c832a74f1d275495d078.

--- notebooks/Test.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, IntegerType, StringType
-
-spark = (SparkSession.builder
-         .appName("WSL-driver")
-         .getOrCreate())
-
-df = spark.range(10)
-df.show()
